900b610eb7269003ae392c18c0771e28d127af07
ljy <junyong.li.121@gmail.com>
2023-01-30 14:18:12 +0100

    Add start shell script

--- lab1example/start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-java mcgui.Main lab1example.ExampleCaster 0 localhostsetup
-java mcgui.Main lab1example.ExampleCaster 1 localhostsetup
-java mcgui.Main lab1example.ExampleCaster 2 localhostsetup
---
+++ start.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+java mcgui.Main lab1example.ExampleCaster 0 ./lab1example/localhostsetup &
+java mcgui.Main lab1example.ExampleCaster 1 ./lab1example/localhostsetup &
+java mcgui.Main lab1example.ExampleCaster 2 ./lab1example/localhostsetup &
